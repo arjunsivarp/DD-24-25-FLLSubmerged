418b9b8ca40ababb651ffba7fa60c301a5456202
Arjun Sivaraj <arjunsivarp@gmail.com>
2025-03-24 23:40:41 -0400

    first commit

---
+++ dd-submerged-v1.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python3
+
+import hub
+import sys
+import time
+
+import color, motor, motor_pair, runloop
+from hub import light_matrix, button, motion_sensor, light, port
+
+
+# CONSTANTS
+#----------------------------------------
+
+WHEEL_CIRCUMFERENCE = 19.2
+
+# END CONSTANTS
+#----------------------------------------
+
+
+# UTILITY FUNCTIONS
+#----------------------------------------
+
+# initialize motor and reset yaw
+def do_init():
+    # reset yaw to 0
+    motion_sensor.set_yaw_face(motion_sensor.TOP)
+    motion_sensor.reset_yaw(0)
+    i = 0
+    while (hub.motion_sensor.stable() == False):
+        i = i + 1
+        # Use time.sleep_ms instead of time.sleep_ms
+        # to ensure it is synchronized
+        time.sleep_ms(10)
+        hub.light_matrix.write(str(i))
+        if i >= 100:
+            break
+
+
+# Return true if LEFT button is pressed
+def is_left_button_pressed():
+    return button.pressed(button.LEFT) > 0
+
+
+# Return true if RIGHT button is pressed
+def is_right_button_pressed():
+    return button.pressed(button.RIGHT) > 0
+
+
+def follow_for_distance(initial_position=0,
+                        distance_to_cover=0):
+    current_position = abs(motor.relative_position(port.A))
+    distance_covered = current_position - initial_position
+    if distance_covered < 0 : distance_covered = distance_covered * -1
+    if (distance_covered >= abs(distance_to_cover)):
+        return False
+    else:
+        return True
+
+
+def get_yaw_value():
+    return motion_sensor.tilt_angles()[0] * -0.1
+
+
+def degrees_for_distance(distance_cm):
+    # Add multiplier for gear ratio if needed
+    return int((distance_cm/WHEEL_CIRCUMFERENCE) * 360)
+
+
+def wait_for_yaw_abs(angle=0):
+    abs_angle = abs(angle)
+    abs_current_yaw = abs(get_yaw_value())
+    if angle == 0:
+        if get_yaw_value() > 0:
+            while get_yaw_value() >= angle: time.sleep_ms(10)
+        elif get_yaw_value() < 0:
+            while get_yaw_value() <= angle: time.sleep_ms(10)
+    elif abs_current_yaw > abs_angle:
+        while abs(get_yaw_value()) >= abs_angle: time.sleep_ms(10)
+    elif abs_current_yaw < abs_angle:
+        while abs(get_yaw_value()) <= abs_angle: time.sleep_ms(10)
+
+
+async def follow_gyro_angle(kp,
+                            ki,
+                            kd,
+                            speed,
+                            target_angle,
+                            sleep_time,
+                            follow_for, **kwargs):
+    # get initial reading from left motor
+    integral = 0.0
+    last_error = 0.0
+    derivative = 0.0
+    while (follow_for(**kwargs)):
+        current_angle = get_yaw_value()
+        error = current_angle - target_angle
+        integral = integral + error
+        derivative = error - last_error
+        last_error = error
+        # compute steering correction
+        steering_value = (error * kp) + (integral * ki) + (derivative * kd)
+
+        if sleep_time:
+            time.sleep_ms(sleep_time)
+        # kp value should be +ve for forward movement (positive speed value), and -ve for backward movement (negative speed value)
+        motor_pair.move(motor_pair.PAIR_1, int(steering_value), velocity=speed)
+
+    # stop when follow_for condition is met
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.HOLD)
+
+
+async def pivot_gyro_turn_abs(left_speed=0, right_speed=50, angle=90, stop=False):
+    motor_pair.move_tank(motor_pair.PAIR_1, left_speed, right_speed)
+    wait_for_yaw_abs(angle=angle)
+    if stop: motor_pair.stop(motor_pair.PAIR_1, stop=motor.HOLD)
+
+
+def get_yaw_angle():
+    current_yaw = motion_sensor.tilt_angles()[0] * -0.1
+    if (current_yaw < 0):
+        return (current_yaw + 360)
+    return current_yaw
+
+async def turnRight(angle):
+    motor_pair.move_tank(motor_pair.PAIR_1, 200, -200)
+    while abs(get_yaw_angle()) <= angle: time.sleep_ms(10)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.HOLD)
+
+async def turnLeft(angle):
+    motor_pair.move_tank(motor_pair.PAIR_1, -200, 200)
+    while abs(get_yaw_angle()) >= angle: time.sleep_ms(10)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.HOLD)
+
+
+def get_time_taken_in_seconds(start_time, end_time):
+    return int(time.ticks_diff(end_time, start_time)/1000)
+
+# END UTILITY FUNCTIONS
+#----------------------------------------
+
+#----------------------------------------
+# RUN 4
+#----------------------------------------
+# run 4 program
+async def run4():
+
+    # go backward to get out of base    # go backward to get out of base
+    motor.reset_relative_position(port.A, 0)
+    initial_position = abs(motor.relative_position(port.A))
+    await follow_gyro_angle(kp=1.45, ki=0, kd=0, speed=-500, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+            initial_position=initial_position, distance_to_cover=(degrees_for_distance(3)))
+
+    # turn left to get in alignment with krill
+    await pivot_gyro_turn_abs(left_speed=-200, right_speed=200, angle=-45, stop=True)
+
+
+    # turn right to get in alignment with coral piece
+    await pivot_gyro_turn_abs(left_speed=200, right_speed=-200, angle=0, stop=True)
+
+
+    # go forward to leave pieces for shipping lanes
+    motor.reset_relative_position(port.A, 0)
+    initial_position = abs(motor.relative_position(port.A))
+    await follow_gyro_angle(kp=-1.45, ki=0, kd=0, speed=600, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+            initial_position=initial_position, distance_to_cover=(degrees_for_distance(12)))
+
+
+    # raise shipping lane/seabed attachment to lift shipping lanes
+    await motor.run_for_degrees(port.C, 1350, 1100)
+
+   
+    # go forward to hook into plankton
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degrees_for_distance(12),0,velocity=300)
+
+    # go backward to pull plankton
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degrees_for_distance(2),0,velocity=-300)
+
+
+    # turn to align to seabed sample
+    await pivot_gyro_turn_abs(left_speed=300, right_speed=-300, angle=0, stop=True)
+    await turnRight(8)
+
+    # raise seabed sample hook to raise the sample and collect it
+    # raise send over the submersible attachment
+    motor.run_for_degrees(port.C, 1200, 900)
+    await motor.run_for_degrees(port.B, 1700, -1000)
+    await runloop.sleep_ms(500)
+    motor.run_for_degrees(port.C, 800, 900)
+
+# END RUN 4
+#----------------------------------------
+
+#----------------------------------------
+# RUN 5
+#----------------------------------------
+# run 5 program
+async def run5():
+
+    # go backward to get out of base    # go backward to get out of base
+    motor.reset_relative_position(port.A, 0)
+    initial_position = abs(motor.relative_position(port.A))
+    await follow_gyro_angle(kp=1.45, ki=0, kd=0, speed=-500, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+            initial_position=initial_position, distance_to_cover=(degrees_for_distance(3)))
+
+    # turn left to get in alignment with krill
+    await pivot_gyro_turn_abs(left_speed=-200, right_speed=200, angle=-45, stop=True)
+
+
+    # turn right to get in alignment with coral piece
+    await pivot_gyro_turn_abs(left_speed=200, right_speed=-200, angle=0, stop=True)
+
+
+    # go forward to leave pieces for shipping lanes
+    motor.reset_relative_position(port.A, 0)
+    initial_position = abs(motor.relative_position(port.A))
+    await follow_gyro_angle(kp=-1.45, ki=0, kd=0, speed=600, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+            initial_position=initial_position, distance_to_cover=(degrees_for_distance(12)))
+
+
+# END RUN 5
+#----------------------------------------
+
+
+
+# MAIN EXECUTE FUNCTION
+#----------------------------------------
+
+async def execute(run_numbers=None):
+
+    runs_to_execute = list()
+
+    if isinstance(run_numbers, int):
+        run_numbers = [run_numbers]
+
+    # If run_numbers are not provided execute all runs
+    runs_to_execute = run_numbers if run_numbers else [1, 2, 3, 4, 5]
+
+    start_times = [time.ticks_ms() for _ in runs_to_execute]
+    end_times = [time.ticks_ms() for _ in runs_to_execute]
+
+    run_functions_map = {
+                            1: run1, # or run1a
+                            2: run2,
+                            3: run3,
+                            4: run4,
+                            5: run5
+                        }
+    print("Start - Execute")
+
+    # Initialization
+    # Define motor pair for robot movements
+    motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
+
+    do_init()
+    light_matrix.write("0")
+    light.color(light.POWER, color.RED)
+
+    for i, run_number in enumerate(runs_to_execute):
+
+        # waiting for left button to be pressed to start the run
+        await runloop.until(is_left_button_pressed)
+        print("Starting Run: " + str(run_number))
+
+        light.color(light.POWER, color.MAGENTA)
+        light_matrix.show_image(light_matrix.IMAGE_BUTTERFLY)
+
+        start_times[i] = time.ticks_ms()
+        do_init()
+
+        runloop.run(run_functions_map[run_number]())
+        end_times[i] = time.ticks_ms()
+        light.color(light.POWER, color.YELLOW)
+
+        if i > 0:
+            print("Transition time: " + str(get_time_taken_in_seconds(end_times[i - 1], start_times[i])) + " s")
+        print("Run " + str(run_number) + " time " + str(get_time_taken_in_seconds(start_times[i], end_times[i])) + " s")
+        print("---------------------------------------------------------------------------")
+
+    # Print execution times
+    print("---------------------------------------------------------------------------")
+    print("SUMMARY:")
+    total_runs_time = 0
+    total_transitions_time = 0
+    total_time = 0
+
+    for i, run_number in enumerate(runs_to_execute):
+        if i > 0:
+            transition_time = get_time_taken_in_seconds(end_times[i - 1], start_times[i])
+            print("Transition time: " + str(transition_time) + " s")
+            total_transitions_time += transition_time
+            total_time += transition_time
+
+        run_time = get_time_taken_in_seconds(start_times[i], end_times[i])
+        print("Run " + str(run_number) + " time " + str(run_time) + " s")
+        total_runs_time += run_time
+        total_time += run_time
+
+    print("***************************************************************************")
+
+    print("TOTAL RUN TIME = " + str(total_runs_time) + " s")
+    print("TOTAL TRANSITIONS TIME = " + str(total_transitions_time) + " s")
+    print("TOTAL TIME = " + str(total_transitions_time + total_runs_time) + " s")
+
+    print("***************************************************************************")
+
+
+# END MAIN EXECUTE FUNCTION
+#----------------------------------------
+# Integrated Runs
+
+# SLOT 0 - All Runs#
+#runloop.run(execute([1, 2, 3, 4, 5]))
+
+# SLOT 1 - Run 2 Onwards
+# runloop.run(execute([2, 3, 4, 5]))
+
+# SLOT 2 - Run 3 Onwards
+# runloop.run(execute([3, 4, 5]))
+
+# SLOT 3 - Run 4 Onwards
+runloop.run(execute([4, 5]))
+
+# SLOT 4 - Run 5
+ # runloop.run(execute([5]))
